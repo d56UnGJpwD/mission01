667ae9dcb540a4c26f87a34f0f767dee622cef66
Kirill Brainard <braikiri@isu.edu>
2019-01-17 14:50:26 -0700

    Singly linked list interfaced

--- src/main/java/edu/isu/cs/cs3308/structures/impl/SinglyLinkedList.java
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/SinglyLinkedList.java
@@ -1,4 +1,69 @@
 package edu.isu.cs.cs3308.structures.impl;
 
-public class SinglyLinkedList {
+import edu.isu.cs.cs3308.structures.List;
+
+public class SinglyLinkedList<E> implements List<E> {
+    /**
+     * E for Element
+     */
+    @Override
+    public E first() {
+        return null;
+    }
+
+    @Override
+    public E last() {
+        return null;
+    }
+
+    @Override
+    public void addLast(E element) {
+
+    }
+
+    @Override
+    public void addFirst(E element) {
+
+    }
+
+    @Override
+    public E removeFirst() {
+        return null;
+    }
+
+    @Override
+    public E removeLast() {
+        return null;
+    }
+
+    @Override
+    public void insert(E element, int index) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void printList() {
+
+    }
+
 }

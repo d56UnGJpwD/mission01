03f9042c0a9b05366b72cc3af5e5eb34511c15ef
Kirill Brainard <braikiri@isu.edu>
2019-01-18 12:58:44 -0700

    Most of the singly linked list is complete

--- src/main/java/edu/isu/cs/cs3308/structures/impl/SinglyLinkedList.java
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/SinglyLinkedList.java
@@ -1,39 +1,112 @@
 package edu.isu.cs.cs3308.structures.impl;
 
+import edu.isu.cs.cs3308.GameEntry;
 import edu.isu.cs.cs3308.structures.List;
 
 public class SinglyLinkedList<E> implements List<E> {
     /**
-     * E for Element
+     *
+     *
+     * This is my node class, every node points to the one in front of it, but not the one
+     * behind it.
      */
+
+    public class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        public Node(E value, Node<E> next){
+            this.value = value;
+        }
+
+        public void setNext(Node<E> next){
+            this.next = next;
+        }
+
+        public Node<E> getNext(){
+            return next;
+        }
+
+        public E getValue(){
+            return value;
+        }
+    }
+    public Node<E> head = null;
+    public Node<E> tail = null;
+    private int size = 0;
+
+    public SinglyLinkedList(){}
+
+
+
+// get the first node of the list
     @Override
     public E first() {
-        return null;
+        if(isEmpty()){
+            return null;
+        }
+        return head.getValue();
     }
-
+// get the last node of the list
     @Override
     public E last() {
-        return null;
+        if(isEmpty()){
+            return null;
+        }
+        return tail.getValue();
     }
-
+// add a node to the end of the list
     @Override
     public void addLast(E element) {
-
+        Node<E> newNode = new Node<>(element, null);
+        if(isEmpty()){
+            head = newNode;
+        }
+        else{
+            tail.setNext((newNode));
+        }
+        tail = newNode;
+        size++;
     }
-
+// add a node to the start of the list
     @Override
     public void addFirst(E element) {
-
+        head = new Node<>(element, head);
+        if(size == 0){
+            tail = head;
+        }
+        size++;
     }
 
     @Override
     public E removeFirst() {
-        return null;
+        if(isEmpty()){
+            return null;
+        }
+        E firstVal = head.getValue();
+        head = head.getNext();
+        size--;
+        if(size == 0){
+            tail = null;
+        }
+        return firstVal;
     }
 
     @Override
     public E removeLast() {
-        return null;
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            head = null;
+            size--;
+        }
+        Node<E> secondLast = head;
+        while(secondLast.next.next != null){
+            secondLast = secondLast.next;
+        }
+        secondLast.next = null;
+        return secondLast.getValue();
     }
 
     @Override
@@ -48,17 +121,26 @@ public class SinglyLinkedList<E> implements List<E> {
 
     @Override
     public E get(int index) {
+        Node<E> current = head;
+        int i = 0;
+        while(current != null){
+            if(i == index){
+                return current.getValue();
+            }
+            i++;
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
